2003611cb2a4cbe15d0404ca3a75c2b667ee3747
Meegeraa <215748@edu.fa.ru>
2022-10-31 00:48:41 +0300

    first commit

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
+import numpy as np
+from multiprocessing import Pool
+
+def write(matrix):
+    #import numpy as np
+    matrix = np.array(matrix).reshape(len(reader_matrix(1)), len(reader_matrix(2)[1]))
+    with open('result.txt', 'a') as f:
+        f.write(str(matrix) + " ")
+
+def multiplication(x, y):
+    return sum(i*k for i, k in zip(x, y))
+
+def reader_matrix(number):
+    matrix = []
+    with open(f'matrix{number}', 'r') as f:
+        for line in f.readlines():
+            matrix.append(list(map(int, line.split(', '))))
+    return matrix
 
 
-def element(index, A, B):
-    i, j = index
-    res = 0
-    # get a middle dimension
-    N = len(A[0]) or len(B)
-    for k in range(N):
-        res += A[i][k] * B[k][j]
-    return res
-
-
-matrix1 = [[1, 2], [3, 4]]
-matrix2 = [[2, 0], [1, 2]]
-
-print(element((1, 0), matrix1, matrix2))
+if name == 'main':
+     with Pool(processes=4) as pool:
+          matrix = pool.starmap(multiplication, [(i, k) for i in reader_matrix(1) for k in zip(*reader_matrix(2))])
+          write(matrix)
